b15c19ebeef552a7bfc79395307a09d312db848a
Natu-goitom <nlakiki108@gmail.com>
2022-07-22 15:29:08 -0700

    second one

--- lib/main.dart
+++ lib/main.dart
@@ -10,19 +10,28 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       title: 'Auth Screen 1',
       theme: ThemeData(
-        brightness:Brightness.dark,
+        brightness: Brightness.dark,
         primaryColor: kPrimaryColor,
         scaffoldBackgroundColor: kBackgroundColor,
-        textTheme:TextTheme(
-          headline4: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,
-         ),
-         button: TextStyle(color: kPrimaryColor,
-         ),
-         headline1:TextStyle(color:Colors.white, fontWeight: FontWeight.normal), 
-         ),
+        textTheme: TextTheme(
+          headline4: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+          button: TextStyle(
+            color: kPrimaryColor,
+          ),
+          headline1:
+              TextStyle(color: Colors.white, fontWeight: FontWeight.normal),
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Colors.white.withOpacity(.2)),
+          ),
+        ),
       ),
       home: WelcomeScreen(),
     );
@@ -35,77 +44,81 @@ class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Column(
+      body: Column(
         children: <Widget>[
           Expanded(
             flex: 3,
-            child:Container(
-              decoration:BoxDecoration(
-               image:DecorationImage(image:AssetImage("assets/perosn.jpeg"),
-            fit:BoxFit.cover,
-            ),
-
-            ),
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/perosn.jpeg"),
+                  fit: BoxFit.cover,
+                ),
+              ),
             ),
           ),
           Expanded(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children:<Widget> [
+              children: <Widget>[
                 RichText(
-                  textAlign:TextAlign.center,
-                  text:TextSpan(
-                 children: [
-                  TextSpan(
-                  text:"BAKING LESSONS\n",
-                  style:Theme.of(context)
-                  .textTheme
-                  .headline4,
-                  
-                 ),
-                 TextSpan(text: "MASTER THE ART OF BAKING",
-                 style: Theme.of(context).textTheme.headline5,
-                 ),
-                 ],
+                  textAlign: TextAlign.center,
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        text: "BAKING LESSONS\n",
+                        style: Theme.of(context).textTheme.headline4,
+                      ),
+                      TextSpan(
+                        text: "MASTER THE ART OF BAKING",
+                        style: Theme.of(context).textTheme.headline5,
+                      ),
+                    ],
                   ),
-                  ),
-                  FittedBox(
-                    child: GestureDetector(
-                      onTap: () {
-                      Navigator.push(context, MaterialPageRoute(
-                        builder: (context) {
-                          return signInScreen();
-                        },
-                      ));
+                ),
+                FittedBox(
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) {
+                            return signInScreen();
+                          },
+                        ),
+                      );
                     },
-                      child: Container(
-                        margin: EdgeInsets.only(bottom: 25),
-                        padding:EdgeInsets.symmetric(horizontal: 26,vertical:16),
-                        decoration: BoxDecoration(borderRadius: BorderRadius.circular(25),
+                    child: Container(
+                      margin: EdgeInsets.only(bottom: 25),
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 26, vertical: 16),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(25),
                         color: kPrimaryColor,
-                        ),
-                        child: Row(
-                          children:<Widget>[
-                            Text(
-                              "START LEARNING",
-                              style:Theme.of(context).textTheme.button?.copyWith(
-                                color:Colors.black,
+                      ),
+                      child: Row(
+                        children: <Widget>[
+                          Text(
+                            "START LEARNING",
+                            style: Theme.of(context).textTheme.button?.copyWith(
+                                  color: Colors.black,
                                 ),
-                              ),
-                              SizedBox(width:10),
-                              Icon(Icons.arrow_forward,
-                                color: Colors.black,
-                              ),
-                          ],
-                        ),
+                          ),
+                          SizedBox(width: 10),
+                          Icon(
+                            Icons.arrow_forward,
+                            color: Colors.black,
+                          ),
+                        ],
                       ),
                     ),
                   ),
+                ),
               ],
             ),
-            ),
+          ),
         ],
       ),
-    );   
+    );
   }
 }
